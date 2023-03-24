158ac543a860586bde17fbb1b82141ad87843ea3
ahmadZaid <ahmaadzaid7@gmail.com>
2023-03-24 13:44:49 +0300

    global styles & custom font

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div class="content">
+    <router-view />
+  </div>
+</template>
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+<style >
+.content {
+  margin: 0 auto;
+  max-width: 1200px;
+  padding: 0 20px;
+}
+</style>
