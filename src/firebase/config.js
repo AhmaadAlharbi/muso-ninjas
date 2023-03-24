548e47bf711d1b3556cfcd2b0c3f8fca3be2fe94
ahmadZaid <ahmaadzaid7@gmail.com>
2023-03-24 15:39:07 +0300

    creating a login view

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyBpYfoD9iuHWmIbvofWVAbyf35XQ0amMTw",
+  authDomain: "muso-ninjas-f236a.firebaseapp.com",
+  projectId: "muso-ninjas-f236a",
+  storageBucket: "muso-ninjas-f236a.appspot.com",
+  messagingSenderId: "824757482955",
+  appId: "1:824757482955:web:467819f3ab594a829b324e",
+};
+
+//init firebase
+firebase.initializeApp(firebaseConfig);
+//init services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+//timestamp
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+export { projectFirestore, projectAuth, timestamp };
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h3>Login</h3>
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Password" v-model="password" />
+    <div v-if="error" class="error">{{ error }}</div>
+    <button>Log in</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+import useLogin from "@/composables/useLogin";
+export default {
+  setup() {
+    const email = ref("");
+    const password = ref("");
+    const { error, login } = useLogin();
+    const handleSubmit = async () => {
+      const res = await login(email.value, password.value);
+      if (!error.value) {
+        console.log("user logged in");
+      }
+    };
+    return { email, password, error, handleSubmit };
+  },
+};
+</script>
+
+<style>
+</style>
